8b3f10e8cffb1d8961131d334cc247a681f508b8
Leonor Da Cunha Domingues <lda-cunh@c2r1s5.42lisboa.com>
2023-05-05 17:01:28 +0100

    gnl done with bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,36 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 18:01:26 by lda-cunh          #+#    #+#             */
-/*   Updated: 2023/05/05 15:14:53 by lda-cunh         ###   ########.fr       */
+/*   Updated: 2023/05/05 15:56:25 by lda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <fcntl.h>
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
+	static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
 	char		*line;
 	int			i;
 
 	i = 0;
 	line = NULL;
-	if (read (fd, 0, 0) < 0 || BUFFER_SIZE <= 0)
+	if (read (fd, 0, 0) < 0 || BUFFER_SIZE < 1 || fd >= FOPEN_MAX)
 	{
-		while (buffer[i])
-			buffer[i++] = 0;
+		if (fd > 0 && fd < FOPEN_MAX)
+		{	
+			while (buffer[fd] && buffer[fd][i])
+				buffer[fd][i++] = 0;
+		}
 		return (NULL);
 	}
-	while (*buffer || read(fd, buffer, BUFFER_SIZE) > 0)
+	while (*(buffer[fd]) || read(fd, buffer[fd], BUFFER_SIZE) > 0)
 	{
-		line = ft_strjoin(line, buffer);
-		if (check_new_line(buffer))
+		line = ft_strjoin(line, buffer[fd]);
+		if (check_new_line(buffer[fd]))
 			break ;
 	}
 	return (line);
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/04 18:02:08 by lda-cunh          #+#    #+#             */
-/*   Updated: 2023/05/05 15:35:01 by lda-cunh         ###   ########.fr       */
+/*   Created: 2023/05/05 15:53:21 by lda-cunh          #+#    #+#             */
+/*   Updated: 2023/05/05 15:54:27 by lda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
 # endif
@@ -19,6 +19,8 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <fcntl.h>
+# include <stdint.h>
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *line, char *buffer);
